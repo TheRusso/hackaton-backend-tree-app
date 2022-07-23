f01123d34b4e2de788bca4f20334d7a817419378
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2022-07-23 22:01:53 +0300

    Fixed wrong annotation in TreeController.

--- src/main/java/com/it/revolution/trees/app/controllers/TreeController.java
+++ src/main/java/com/it/revolution/trees/app/controllers/TreeController.java
@@ -4,11 +4,9 @@ import com.it.revolution.trees.app.model.dto.TreeDto;
 import com.it.revolution.trees.app.model.dto.TreeShortDto;
 import com.it.revolution.trees.app.service.TreeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
@@ -21,10 +19,10 @@ public class TreeController {
     private final TreeService treeService;
 
     @GetMapping
-    public ResponseEntity<List<TreeShortDto>> getListTreeShort(@PathParam("startX") Double startX,
-                                                               @PathParam("endX") Double endX,
-                                                               @PathParam("startY") Double startY,
-                                                               @PathParam("endY") Double endY) {
+    public ResponseEntity<List<TreeShortDto>> getListTreeShort(@RequestParam("startX") Double startX,
+                                                               @RequestParam("endX") Double endX,
+                                                               @RequestParam("startY") Double startY,
+                                                               @RequestParam("endY") Double endY) {
         List<TreeShortDto> trees = treeService.getTreeShortList(startX, endX, startY, endY);
         return ResponseEntity.ok(trees);
     }
