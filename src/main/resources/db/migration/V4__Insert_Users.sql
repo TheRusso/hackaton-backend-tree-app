21cbae688846fe27191e5781c120a195fae50af5
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2022-07-24 00:25:26 +0300

    Added activation code attribute.

--- src/main/resources/db/migration/V4__Insert_Users.sql
+++ src/main/resources/db/migration/V4__Insert_Users.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (id, email, password, full_name) VALUES (1, 'kovalchuk.roman03@gmail.com', '$2a$08$/DS9cBHk4xe/RurfMYmrC.ubIi6apUJczAlyMjJIrcukUuPNQKYCa', 'Ковальчук Роман Олександрович');
-INSERT INTO users (id, email, password, full_name) VALUES (2, 'podhorhyi_2002@ukr.net', '$2a$08$iTxDMNrWjSQ4WSkhqk4LjOWt5MC2PG/12HgPlmW9pJJn12IUCT4iW', 'Підгорний Ігор Миколайович');
+INSERT INTO users (id, email, password, full_name, activation_code) VALUES (1, 'kovalchuk.roman03@gmail.com', '$2a$08$/DS9cBHk4xe/RurfMYmrC.ubIi6apUJczAlyMjJIrcukUuPNQKYCa', 'Ковальчук Роман Олександрович', '');
+INSERT INTO users (id, email, password, full_name, activation_code) VALUES (2, 'podhorhyi_2002@ukr.net', '$2a$08$iTxDMNrWjSQ4WSkhqk4LjOWt5MC2PG/12HgPlmW9pJJn12IUCT4iW', 'Підгорний Ігор Миколайович', '');
 
 ALTER SEQUENCE users_id_seq RESTART WITH 3;
 
